5fe09a9dadcc5d57cf0416ba125ae1b0e26a08fe
Dipesh <dipesh.sakharkar@otiselevator.com>
2020-07-12 19:51:20 +0530

    Icon and splash sceen and status bar

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,StatusBar } from 'react-native';
+
 import NavigationRoute from "./NavigationRoute";
 
 export default function App() {
   return (
+    <View style={styles.container}>
+       <StatusBar barStyle="default" hidden={false} backgroundColor="#00a08c" translucent={false}/>
        <NavigationRoute style={styles.container}/>
+      </View>
   );
 }
 
